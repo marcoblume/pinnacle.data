82a7bc72e0b46b65122336da016998d01bded05c
Marco Blume <marcob@pinnaclesports.com>
2017-03-27 12:04:04 -0400

    Fixed Timezones in data

--- R/data.R
+++ R/data.R
@@ -1,17 +1,19 @@
 #' MLB2016.
 #'
+#' MLB data 2016.
+#'
 #' All wagering lines from Pinnacle for the 2016  MLB season
 #'
 #' @format A data frame with 17 variables:
 #' \describe{
 #' \item{\code{EventDateTime}}{Time of the game in UTC}
 #' \item{\code{LineId}}{Internal LineID}
-#' \item{\code{TeamName1}}{Teamname of the Away Team}
-#' \item{\code{TeamName1}}{Teamname of the Home Team}
+#' \item{\code{TeamName1}}{Team name of the Away Team}
+#' \item{\code{TeamName1}}{Team name of the Home Team}
 #' \item{\code{StartingPitcher1}}{Starting pitcher Away Team}
 #' \item{\code{StartingPitcher2}}{Starting pitcher Home Team}
-#' \item{\code{TeamName1}}{Teamname of the Away Team}
-#' \item{\code{TeamName2}}{Teamname of the Home Team}
+#' \item{\code{TeamName1}}{Team name of the Away Team}
+#' \item{\code{TeamName2}}{Team name of the Home Team}
 #' \item{\code{GameScoreTeam1}}{Number of runs for Away Team}
 #' \item{\code{GameScoreTeam2}}{Number of runs for Home Team}
 #' \item{\code{EnteredDateTime}}{Time of the wager line in UTC}
@@ -20,8 +22,8 @@
 #' \item{\code{SpreadUS2}}{Spread US odds for Home Team}
 #' \item{\code{MoneyUS1}}{Moneyline US odds for Away Team}
 #' \item{\code{MoneyUS2}}{Moneyline US odds for Home Team}
-#' \item{\code{TotalPoints}}{Totalruns handicap}
-#' \item{\code{TotalUSOver}}{Totalruns US odds for Over}
-#' \item{\code{TotalUSUnder}}{Totalruns US odds for Under}
+#' \item{\code{TotalPoints}}{Total runs handicap}
+#' \item{\code{TotalUSOver}}{Total runs US odds for Over}
+#' \item{\code{TotalUSUnder}}{Total runs US odds for Under}
 #' }
 "MLB2016"
