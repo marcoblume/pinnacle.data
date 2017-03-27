536a046dd6a2f0edfe83faae3cd1d0dac1a26534
Marco Blume <marcob@pinnaclesports.com>
2017-03-27 11:39:34 -0400

    Changed Description

--- R/data.R
+++ R/data.R
@@ -3,15 +3,25 @@
 #' All wagering lines from Pinnacle for the 2016  MLB season
 #'
 #' @format A data frame with 17 variables:
-#' \code{EventDateTime}, \code{LineId},
-#' \code{TeamName1}, \code{TeamName2},
-#'  \code{StartingPitcher1}, \code{StartingPitcher2},
-#'   \code{GameScoreTeam1}, \code{GameScoreTeam2},
-#'  \code{EnteredDateTime}, \code{SpreadTeam1},
-#'   \code{SpreadUS1}, \code{SpreadUS2},
-#'   \code{MoneyUS1}, \code{MoneyUS2},
-#'   \code{TotalPoints}, \code{TotalUSOver},
-#'   \code{TotalUSUnder}
-#'    and \code{prop} (\code{n} divided by total number
-#'   of applicants in that year).
+#' \describe{
+#' \item{\code{EventDateTime}}{Time of the game in UTC}
+#' \item{\code{LineId}}{Internal LineID}
+#' \item{\code{TeamName1}}{Teamname of the Away Team}
+#' \item{\code{TeamName1}}{Teamname of the Home Team}
+#' \item{\code{StartingPitcher1}}{Starting pitcher Away Team}
+#' \item{\code{StartingPitcher2}}{Starting pitcher Home Team}
+#' \item{\code{TeamName1}}{Teamname of the Away Team}
+#' \item{\code{TeamName2}}{Teamname of the Home Team}
+#' \item{\code{GameScoreTeam1}}{Number of runs for Away Team}
+#' \item{\code{GameScoreTeam2}}{Number of runs for Home Team}
+#' \item{\code{EnteredDateTime}}{Time of the wager line in UTC}
+#' \item{\code{SpreadTeam1}}{Spread Handicap for Away Team}
+#' \item{\code{SpreadUS1}}{Spread US odds for Away Team}
+#' \item{\code{SpreadUS2}}{Spread US odds for Home Team}
+#' \item{\code{MoneyUS1}}{Moneyline US odds for Away Team}
+#' \item{\code{MoneyUS2}}{Moneyline US odds for Home Team}
+#' \item{\code{TotalPoints}}{Totalruns handicap}
+#' \item{\code{TotalUSOver}}{Totalruns US odds for Over}
+#' \item{\code{TotalUSUnder}}{Totalruns US odds for Under}
+#' }
 "MLB2016"
