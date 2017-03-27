fe9c9dafdead7d55af17432100320650c2b04116
Marco Blume <marcob@pinnaclesports.com>
2017-03-27 10:50:27 -0400

    Update to the .rda

--- R/data.R
+++ R/data.R
@@ -2,7 +2,16 @@
 #'
 #' All wagering lines from Pinnacle for the 2016  MLB season
 #'
-#' @format A data frame with 20 variables: \code{year}, \code{sex},
-#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
+#' @format A data frame with 17 variables:
+#' \code{EventDateTime}, \code{LineId},
+#' \code{TeamName1}, \code{TeamName2},
+#'  \code{StartingPitcher1}, \code{StartingPitcher2},
+#'   \code{GameScoreTeam1}, \code{GameScoreTeam2},
+#'  \code{EnteredDateTime}, \code{SpreadTeam1},
+#'   \code{SpreadUS1}, \code{SpreadUS2},
+#'   \code{MoneyUS1}, \code{MoneyUS2},
+#'   \code{TotalPoints}, \code{TotalUSOver},
+#'   \code{TotalUSUnder}
+#'    and \code{prop} (\code{n} divided by total number
 #'   of applicants in that year).
 "MLB2016"
