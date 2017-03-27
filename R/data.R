e71bab01642cb1b052417dfbe7f931aa36869821
Marco Blume <marcob@pinnaclesports.com>
2017-03-27 10:40:36 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' MLB2016.
+#'
+#' All wagering lines from Pinnacle for the 2016  MLB season
+#'
+#' @format A data frame with 20 variables: \code{year}, \code{sex},
+#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
+#'   of applicants in that year).
+"MLB2016"
