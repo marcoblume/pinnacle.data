a3d52fbac7f586f43006012e3c658817567868c0
Michael Yan <michael.yan@crescendotechnology.com>
2017-06-23 14:09:24 +0000

    update Election 2016 data from different source

--- R/data.R
+++ R/data.R
@@ -26,18 +26,17 @@
 "MLB2016"
 
 
-#' Election2016
+#' USA_Election_2016
 #'
-#' US Election data 2016.
+#' US Presidential Election data 2016.
 #'
-#' All wagering lines from Pinnacle for the 2016 US Presidential Election
+#' All lines from Pinnacle for the 2016 US Presidential Election
 #'
 #' @format A data frame with 12 variables:
 #' \describe{
-#' \item{\code{EventDateTime}}{Time of the game in UTC}
+#' \item{\code{EnteredDateTime}}{Time of the wager line in UTC}
 #' \item{\code{TeamName1}}{Team name of the Away Team}
 #' \item{\code{TeamName2}}{Team name of the Home Team}
-#' \item{\code{EnteredDateTime}}{Time of the wager line in UTC}
 #' \item{\code{SpreadTeam1}}{Spread Handicap for Away Team}
 #' \item{\code{SpreadUS1}}{Spread US odds for Away Team}
 #' \item{\code{SpreadUS2}}{Spread US odds for Home Team}
@@ -47,4 +46,4 @@
 #' \item{\code{TotalUSOver}}{Total runs US odds for Over}
 #' \item{\code{TotalUSUnder}}{Total runs US odds for Under}
 #' }
-"Election2016"
+"USA_Election_2016"
