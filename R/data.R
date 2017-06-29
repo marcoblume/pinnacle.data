e2d0ebd65e8adb1af72d48dc90c048c6d45c67af
Marco Blume <marco.blume@pinnacle.com>
2017-06-29 09:01:22 -0400

    More Documentation changes

--- R/data.R
+++ R/data.R
@@ -32,8 +32,9 @@
 #' }
 #' 
 #' @examples 
-#' # What was the range of expected total runs according to the prediction market at Pinnacle?
-#'library(dplyr)
+#' if (require("tidyverse")) {
+#' library(tidyverse)
+ #' # What was the range of expected total runs according to the prediction market at Pinnacle?
 #'MLB2016 %>% 
 #'  unnest() %>% 
 #'  group_by(GameID) %>% 
@@ -42,26 +43,25 @@
 #'  ungroup() %>% 
 #'  group_by(TotalPoints) %>% 
 #'  summarize(Count = n())
-#'  
-#'  
-#'  ' # How many games went over/under or landed on the predicated total?
-#'library(dplyr)
-#' MLB2016 %>% 
-#'   unnest() %>% 
-#'   group_by(GameID) %>% 
-#'   arrange(desc(EnteredDateTimeUTC)) %>% 
-#'   slice(1) %>% 
-#'   ungroup() %>% 
-#'   select(GameID,TotalPoints,FinalScoreAway,FinalScoreHome) %>% 
-#'   mutate(TotalOutcome = case_when(
-#'     FinalScoreAway + FinalScoreHome > TotalPoints ~ "Over",
-#'     FinalScoreAway + FinalScoreHome < TotalPoints ~ "Under",
-#'     FinalScoreAway + FinalScoreHome == TotalPoints ~ "Landed"
-#'   )
-#'   ) %>% 
-#'   group_by(TotalPoints,TotalOutcome) %>% 
-#'   summarize(Count = n())
-#'   
+#' 
+#' # How many games went Over/Under/Landed on the total?
+#'MLB2016 %>% 
+#'  unnest() %>% 
+#'  group_by(GameID) %>% 
+#'  arrange(desc(EnteredDateTimeUTC)) %>% 
+#'  slice(1) %>% 
+#'  ungroup() %>% 
+#'  select(GameID,TotalPoints,FinalScoreAway,FinalScoreHome) %>% 
+#'  mutate(TotalOutcome = case_when(
+#'    FinalScoreAway + FinalScoreHome > TotalPoints ~ "Over",
+#'    FinalScoreAway + FinalScoreHome < TotalPoints ~ "Under",
+#'    FinalScoreAway + FinalScoreHome == TotalPoints ~ "Landed"
+#'  )
+#'  ) %>% 
+#'  group_by(TotalPoints,TotalOutcome) %>% 
+#'  summarize(Count = n()) %>% 
+#'  print(n=100)
+#' }
 "MLB2016"
 
 
